15e746c680de40dfeb03ee75c2b106e11a7af94d
mittal-sakshat <akshumittalgul5670@gmail.com>
2022-12-23 18:46:42 +0530

    commit

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const errorMiddleware = require("./middlerware/error")
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+const dotenv = require('dotenv')
+const path = require("path");
+
+dotenv.config({path:"backend/config/config.env"})
+
+
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser());
+app.use(fileUpload());
+
+//route imports
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+
+
+app.use("/api/v1",product);
+app.use("/api/v1",user);
+app.use("/api/v1",order);
+app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+
+//middleware for errors
+
+app.use(errorMiddleware)
+
+module.exports = app
---
+++ backend/middlerware/catchAsyncErrors.js
@@ -0,0 +1,4 @@
+module.exports = thefun =>(req,res,next) => {
+
+    Promise.resolve(thefun(req,res,next)).catch(next)
+}
